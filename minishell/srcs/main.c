100731edbb5fa6f59bbaf9257b6bce8626f214b2
Frederick Moor <fwmoor@Fredericks-MacBook-Pro.local>
2019-07-21 15:25:25 +0200

    changed some colours

--- minishell/srcs/main.c
+++ minishell/srcs/main.c
@@ -5,7 +5,7 @@ int main(){
 	system("@cls||clear");
 	while (1)
 	{
-		ft_printf("$> ");
+		ft_printf("\033[32;1m$>\033[0m ");
 		get_next_line(0, &line);
 		if (ft_strcmp(line, "exit") != 0)
 			ft_printf("%s\n", line);
