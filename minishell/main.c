4b707a127f3c143c95a9d335dc176d81d9daccb2
fwmoor <fwmoor@outlook.com>
2019-07-21 10:37:54 +0200

    still just a gnl lmao

---
+++ minishell/main.c
@@ -0,0 +1,15 @@
+#include "./libft/includes/libft.h"
+
+int main(){
+	char *line;
+
+	while (1)
+	{
+		ft_printf("$> ");
+		get_next_line(0, &line);
+		if (ft_strcmp(line, "quit;") != 0)
+			ft_printf("%s\n", line);
+		else
+			exit (1);
+	}
+}
