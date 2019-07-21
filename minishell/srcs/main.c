5d182d023d48f630c331bc359c614fabccd34cca
fwmoor <fwmoor@outlook.com>
2019-07-21 11:01:52 +0200

    changed quit to exit

--- minishell/srcs/main.c
+++ minishell/srcs/main.c
@@ -2,12 +2,12 @@
 
 int main(){
 	char *line;
-
+	system("@cls||clear");
 	while (1)
 	{
 		ft_printf("$> ");
 		get_next_line(0, &line);
-		if (ft_strcmp(line, "quit;") != 0)
+		if (ft_strcmp(line, "exit") != 0)
 			ft_printf("%s\n", line);
 		else
 			exit (1);
