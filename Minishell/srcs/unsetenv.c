003461dd98544e2628e266b13059b92db1ce5f7b
fwmoor <fwmoor@outlook.com>
2019-08-05 14:23:25 +0200

    working unsetenv

--- Minishell/srcs/unsetenv.c
+++ Minishell/srcs/unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: fremoor <fremoor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 11:56:08 by fremoor           #+#    #+#             */
-/*   Updated: 2019/08/05 12:26:52 by fremoor          ###   ########.fr       */
+/*   Updated: 2019/08/05 14:22:22 by fremoor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,26 @@
 int			unsetenv_var(char *key)
 {
 	int		i;
+	int		j;
+	int		len;
 	int		key_ind;
-	char	*temp_key;
 	char	**temp;
 
 	i = 0;
+	j = 0;
+	len = 0;
 	if ((key_ind = get_envind(key)) == -1)
 		return (0);
-	while (g_env[i])
-		i++;
-	temp = (char **)malloc(sizeof(char *) * (i + 1));
-	temp[i] = 0;
+	while (g_env[len])
+		len++;
+	temp = (char **)malloc(sizeof(char *) * (len + 1));
+	temp[len] = 0;
 	if (temp)
 	{
-		i = 0;
-		while (g_env[i])
+		while (i < len && g_env[i])
 		{
 			if (i != key_ind)
-				temp[i] = ft_strdup(g_env[i]);
+				temp[j++] = ft_strdup(g_env[i]);
 			i++;
 		}
 		free_her(g_env);
